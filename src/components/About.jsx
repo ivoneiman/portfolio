331b19f1c784ae0364069becdb3827e36848e012
Ivo <ivoneiman@gmail.com>
2024-12-26 23:41:53 -0300

    Actualización del portfolio: cambios en About Me, íconos de redes sociales y favicon

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import {ABOUT_TEXT} from "../constants";
 const About = () => {
   return <div className="border-b border-neutral-900 pb-4">
         <h2 className="my-20 text-center text-4xl">
-            About <span className="text-neutral-500">Me</span>
+            Read <span className="text-neutral-500">a bit</span>
         </h2>
         <div className="flex flex-wrap items-center">
             <div className="w-full lg:w-1/2 lg:p-8">
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,30 @@
 import logo from "../assets/ivoNeimanLogo.png"
-import {FaLinkedin} from "react-icons/fa"
-import {FaGithub} from "react-icons/fa"
-import {FaInstagram} from "react-icons/fa"
-import {FaUpwork} from "react-icons/fa6"
-
+import { FaLinkedin } from "react-icons/fa"
+import { FaGithub } from "react-icons/fa"
+import { FaInstagram } from "react-icons/fa"
+import { FaUpwork } from "react-icons/fa6"
 
 const Navbar = () => {
-  return <nav className="mb-2 flex items-center justify-between py-6">
-        <div className="flex flex-shrink-0 items-center">
-            <img className="mx-2 w-12" src={logo} alt="logo" />
-        </div>
-        <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-            <FaLinkedin />
-            <FaGithub />
-            <FaInstagram />
-            <FaUpwork />
-        </div>
+  return (
+    <nav className="mb-2 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <img className="mx-2 w-12" src={logo} alt="logo" />
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a href="https://www.linkedin.com/in/ivoneiman" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com/ivoneiman" target="_blank" rel="noopener noreferrer">
+          <FaGithub />
+        </a>
+        <a href="https://www.instagram.com/ivo.studios/?hl=es" target="_blank" rel="noopener noreferrer">
+          <FaInstagram />
+        </a>
+        <a href="https://www.upwork.com/freelancers/~0144b56e186e59bb83?mp_source=share" target="_blank" rel="noopener noreferrer">
+          <FaUpwork />
+        </a>
+      </div>
     </nav>
+  )
 }
 export default Navbar
